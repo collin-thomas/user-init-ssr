e6c14960de17009972ee4c4115c95ec2d0423736
Collin Thomas <cothomas@bills.com>
2023-06-22 16:07:46 -0700

    Add user input

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,19 @@ import { useRouter } from "next/router";
 import { useState } from "react";
 import Image from "next/image";
 
-export default function Home({ imgUrl }) {
+export default function Home({ imgUrl, defaultBreed }) {
   const router = useRouter();
   const [counter, setCounter] = useState(0);
+  const [breed, setBreed] = useState(defaultBreed);
 
   const refreshData = () => {
-    router.replace(router.asPath);
+    const url = new URL(location.href);
+    if (breed !== "all") {
+      url.searchParams.set("breed", breed);
+    } else {
+      url.searchParams.delete("breed");
+    }
+    router.replace(url);
   };
 
   const increment = () => {
@@ -16,31 +23,44 @@ export default function Home({ imgUrl }) {
 
   return (
     <>
-      <h1>{counter}</h1>
       <div>
-      <button type="button" onClick={refreshData}>
-        refresh
-      </button>
-      <button type="button" onClick={increment}>
-        increment
-      </button>
+        <h1>{counter}</h1>
+        <button type="button" onClick={increment}>
+          increment
+        </button>
       </div>
-      <div style={{ position: 'relative',  height: '300px' }}>
-      <Image
-        src={imgUrl}
-        alt="dog"
-        layout="fill"
-        objectFit="contain"
-      />
+
+      <div>
+        <select
+          name="breed"
+          defaultValue={defaultBreed}
+          onChange={(e) => setBreed(e.target.value)}
+        >
+          <option value="all">All</option>
+          <option value="cotondetulear">Coton de Tulear</option>
+          <option value="dalmatian">Dalmatian</option>
+          <option value="shiba">Shiba</option>
+        </select>
+        <button type="button" onClick={refreshData}>
+          refresh
+        </button>
+        <div style={{ position: "relative", height: "300px" }}>
+          <Image src={imgUrl} alt="dog" layout="fill" objectFit="contain" />
+        </div>
       </div>
     </>
   );
 }
 export async function getServerSideProps(context) {
-  const res = await fetch("https://dog.ceo/api/breeds/image/random", {
+  let url = "https://dog.ceo/api/breeds/image/random";
+  const breed = context?.query?.breed;
+  if (breed && breed !== "all") {
+    url = `https://dog.ceo/api/breed/${breed}/images/random`;
+  }
+  const res = await fetch(url, {
     method: "GET",
     cache: "no-cache",
   });
   const { message: imgUrl } = await res.json();
-  return { props: { imgUrl } };
+  return { props: { imgUrl, defaultBreed: breed ? breed : 'all' } };
 }
